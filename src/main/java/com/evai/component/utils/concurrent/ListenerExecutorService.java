998c34379faf7b256969fbfb47b913dba2ed0608
ChenEvai <497983114@qq.com>
2019-12-25 14:12:58 +0800

    优化

--- src/main/java/com/evai/component/utils/concurrent/ListenerExecutorService.java
+++ src/main/java/com/evai/component/utils/concurrent/ListenerExecutorService.java
@@ -3,12 +3,12 @@ package com.evai.component.utils.concurrent;
 import com.evai.component.utils.RandomUtil;
 import com.evai.component.utils.SleepUtil;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 /**
@@ -21,6 +21,7 @@ public class ListenerExecutorService {
     /**
      * 要执行的线程池
      */
+    @Getter
     private ExecutorService executorService;
     /**
      * 成功回调
@@ -31,10 +32,21 @@ public class ListenerExecutorService {
      */
     private Consumer<Throwable> throwableConsumer;
 
+    /**
+     * 任务完成回调
+     */
+    private Runnable finishCallback;
+
     public ListenerExecutorService(ExecutorService executorService) {
         this.executorService = executorService;
     }
 
+    /**
+     * 成功回调
+     *
+     * @param consumer
+     * @return
+     */
     public ListenerExecutorService onSuccess(Consumer<Object> consumer) {
         if (consumer != null) {
             this.vConsumer = consumer;
@@ -42,6 +54,12 @@ public class ListenerExecutorService {
         return this;
     }
 
+    /**
+     * 失败回调
+     *
+     * @param consumer
+     * @return
+     */
     public ListenerExecutorService onFailure(Consumer<Throwable> consumer) {
         if (consumer != null) {
             this.throwableConsumer = consumer;
@@ -49,12 +67,20 @@ public class ListenerExecutorService {
         return this;
     }
 
-    public void submit(Callable<Object> callable) {
-        this.executorService.execute(new CallbackListener(this.executorService.submit(callable), vConsumer, throwableConsumer));
+    /**
+     * 任务完成回调
+     *
+     * @param runnable
+     * @return
+     */
+    public void onFinish(Runnable runnable) {
+        if (runnable != null) {
+            this.finishCallback = runnable;
+        }
     }
 
-    public void execute(Runnable runnable) {
-        this.executorService.execute(new CallbackListener(this.executorService.submit(runnable), vConsumer, throwableConsumer));
+    public void submit(Callable<Object> callable) {
+        this.executorService.execute(new CallbackListener(callable, vConsumer, throwableConsumer, finishCallback));
     }
 
     public void shutdown() {
@@ -65,15 +91,16 @@ public class ListenerExecutorService {
 
     @AllArgsConstructor
     private static final class CallbackListener implements Runnable {
-        final Future<?> future;
-        final Consumer<Object> vConsumer;
-        final Consumer<Throwable> throwableConsumer;
+        private final Callable<?> callable;
+        private final Consumer<Object> vConsumer;
+        private final Consumer<Throwable> throwableConsumer;
+        private final Runnable finishCallback;
 
         @Override
         public void run() {
             Throwable cause;
             try {
-                Object value = future.get();
+                Object value = callable.call();
                 if (vConsumer != null) {
                     vConsumer.accept(value);
                 }
@@ -88,6 +115,10 @@ public class ListenerExecutorService {
                 }
             } catch (Throwable e) {
                 cause = e;
+            } finally {
+                if (finishCallback != null) {
+                    finishCallback.run();
+                }
             }
             if (throwableConsumer != null) {
                 throwableConsumer.accept(cause);
@@ -97,26 +128,77 @@ public class ListenerExecutorService {
     }
 
     public static void main(String[] args) {
-        log.info("start");
+//        Thread thread = new Thread(new Task());
+//        thread.run();
+//        SleepUtil.milliseconds(200);
+//        thread.interrupt();
+        ListenerExecutorService listenerExecutorService = new ListenerExecutorService(ThreadPoolUtil.newFixedThreadPool("test", 10));
         long start = System.currentTimeMillis();
-        ExecutorService executorService = ThreadPoolUtil.newFixedThreadPool(Runtime
-                .getRuntime()
-                .availableProcessors(), "test");
-        ListenerExecutorService listenerExecutorService = new ListenerExecutorService(executorService)
-                .onSuccess(v -> log.info("执行成功: {}", v))
-                .onFailure(e -> log.warn(e.toString()));
-        for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 100; j++) {
+            listenerExecutorService
+                    .onSuccess(res -> log.info("执行成功: {}", res))
+                    .onFailure(err -> log.warn(err.toString()));
             listenerExecutorService.submit(() -> {
                 int n = RandomUtil.getRandomInt(100);
-                SleepUtil.seconds(RandomUtil.getRandomInt(0, 1));
+                SleepUtil.milliseconds(100);
                 if (n % 2 == 0) {
-                    int j = n / 0;
+                    int k = n / 0;
                 }
                 return n;
             });
         }
         listenerExecutorService.shutdown();
+        ThreadPoolUtil.await(listenerExecutorService.getExecutorService());
         log.info("end, time={}", System.currentTimeMillis() - start);
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            log.info("start--->{}", Thread.currentThread().getName());
+
+            ExecutorService service = ThreadPoolUtil.newSingleThreadExecutor("single");
+            service.execute(() -> {
+                long start = System.currentTimeMillis();
+                ListenerExecutorService listenerExecutorService = new ListenerExecutorService(ThreadPoolUtil.newFixedThreadPool("test", 50));
+                int c = 1;
+                for (int i = 0; i < 1; i++) {
+                    log.info("执行第{}批任务。。。", c);
+//                    CountDownLatch countDownLatch = new CountDownLatch(10);
+                    for (int j = 0; j < 500; j++) {
+                        listenerExecutorService
+                                .onSuccess(res -> log.info("执行成功: {}", res))
+                                .onFailure(err -> log.warn(err.toString()));
+//                                .onFinish(countDownLatch::countDown);
+                        listenerExecutorService.submit(() -> {
+                            int n = RandomUtil.getRandomInt(100);
+                            SleepUtil.milliseconds(100);
+                            if (n % 2 == 0) {
+                                int k = n / 0;
+                            }
+                            return n;
+                        });
+                    }
+
+                    if (Thread.currentThread().isInterrupted()) {
+                        log.info("exit....");
+                        break;
+                    }
+//                    try {
+//                        countDownLatch.await();
+//                    } catch (InterruptedException e) {
+//                        Thread.currentThread().interrupt();
+//                    }
+                    c++;
+                }
+                listenerExecutorService.shutdown();
+                ThreadPoolUtil.await(listenerExecutorService.getExecutorService());
+                log.info("end, time={}", System.currentTimeMillis() - start);
+            });
+            service.shutdown();
+            log.info("end...");
+        }
     }
 
 }
--- src/main/java/com/evai/component/utils/concurrent/ThreadPoolUtil.java
+++ src/main/java/com/evai/component/utils/concurrent/ThreadPoolUtil.java
@@ -1,15 +1,16 @@
 package com.evai.component.utils.concurrent;
 
+import com.evai.component.utils.SleepUtil;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
 
 /**
- * @author: Evai
+ * @author: crh
  * @date: 2018/9/17
  */
 @Slf4j
@@ -20,26 +21,29 @@ public class ThreadPoolUtil {
      * 当线程数小于核心线程数时，即使有线程空闲，线程池也会优先创建新线程处理
      * 设置allowCoreThreadTimeout=true（默认false）时，核心线程会超时关闭
      */
-    public static final int CORE_POOL_SIZE = Runtime
-            .getRuntime()
-            .availableProcessors();
+    public static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors();
     /**
      * 最大线程数
      * 当线程数>=corePoolSize，且任务队列已满时。线程池会创建新线程来处理任务
      * 当线程数=maxPoolSize，且任务队列已满时，线程池会拒绝处理任务而抛出异常
      */
-    public static final int MAX_POOL_SIZE = Integer.MAX_VALUE;
+    public static final int MAX_POOL_SIZE = 500;
     /**
      * 线程空闲时间
      * 当线程空闲时间达到keepAliveTime时，线程会退出，直到线程数量=corePoolSize
      * 如果allowCoreThreadTimeout=true，则会直到线程数量=0
      */
-    public static final long KEEP_ALIVE_TIME = 60L;
+    public static final long KEEP_ALIVE_TIME = 30L;
     /**
      * 任务队列容量（阻塞队列）
      * 当核心线程数达到最大时，新任务会放在队列中排队等待执行
      */
-    public static final int QUEUE_SIZE = 1024;
+    public static final int QUEUE_SIZE = 1000;
+
+    /**
+     * 默认拒绝策略
+     */
+    public static final RejectedExecutionHandler DEFAULT_HANDLER = new ThreadPoolExecutor.AbortPolicy();
 
 
     private ThreadPoolUtil() {
@@ -48,11 +52,41 @@ public class ThreadPoolUtil {
     /**
      * 自定义拒绝策略
      */
-    public static class CustomPolicy implements RejectedExecutionHandler {
+    public static class CustomCallerRunsPolicy implements RejectedExecutionHandler {
         @Override
-        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
-            log.info("任务: {}，参数: {}", r, executor.toString());
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor e) {
+            log.info("线程池任务已满，当前任务: {}，当前线程: {}", r, e);
+            throw new RejectedExecutionException("Task " + r.toString() +
+                    " rejected from " +
+                    e.toString());
         }
+    }
+
+    /**
+     * 自定义线程池
+     *
+     * @param corePoolSize
+     * @param maximumPoolSize
+     * @param keepAliveTime
+     * @param unit
+     * @param queueSize
+     * @param handler
+     * @return
+     */
+    public static ExecutorService newThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, int queueSize, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
+        if (corePoolSize < 1) {
+            corePoolSize = 1;
+        }
+        if (corePoolSize > MAX_POOL_SIZE) {
+            corePoolSize = MAX_POOL_SIZE;
+        }
+        if (maximumPoolSize < 1) {
+            maximumPoolSize = 1;
+        }
+        if (maximumPoolSize > MAX_POOL_SIZE) {
+            maximumPoolSize = MAX_POOL_SIZE;
+        }
+        return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, createQueue(queueSize), threadFactory, handler);
     }
 
     /**
@@ -63,34 +97,46 @@ public class ThreadPoolUtil {
      * @param maximumPoolSize
      * @param keepAliveTime
      * @param unit
-     * @param workQueue
+     * @param queueSize
      * @param handler
      * @return
      */
-    public static ExecutorService newThreadPoolExecutor(String threadPrefix, int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
-            BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
+    public static ExecutorService newThreadPoolExecutor(String threadPrefix, int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, int queueSize, RejectedExecutionHandler handler) {
         ThreadFactory threadFactory = setThreadFactory(threadPrefix);
-        return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+        return newThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, queueSize, threadFactory, handler);
+    }
+
+    private static BlockingQueue<Runnable> createQueue(int queueSize) {
+        return queueSize > 0 ? new LinkedBlockingQueue<>(queueSize) : new SynchronousQueue<>();
+    }
+
+    public static ExecutorService newCachedThreadPool(String threadPrefix) {
+        return newThreadPoolExecutor(threadPrefix, 0, MAX_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, 0, DEFAULT_HANDLER);
     }
 
     /**
      * 创建一个缓存线程池
      */
-    public static ExecutorService newCachedThreadPool(String threadPrefix) {
-        ThreadFactory threadFactory = setThreadFactory(threadPrefix);
-        return Executors.newCachedThreadPool(threadFactory);
+    public static ExecutorService newCachedThreadPool(String threadPrefix, int maximumPoolSize) {
+        return newThreadPoolExecutor(threadPrefix, 1, maximumPoolSize, KEEP_ALIVE_TIME, TimeUnit.SECONDS, QUEUE_SIZE, DEFAULT_HANDLER);
+    }
+
+    /**
+     * 创建一个缓存线程池
+     */
+    public static ExecutorService newCachedThreadPool(String threadPrefix, int corePoolSize, int maximumPoolSize) {
+        return newThreadPoolExecutor(threadPrefix, corePoolSize, maximumPoolSize, KEEP_ALIVE_TIME, TimeUnit.SECONDS, QUEUE_SIZE, DEFAULT_HANDLER);
     }
 
     /**
      * 创建一个指定工作线程数量的线程池
      *
-     * @param nThreads
      * @param threadPrefix
+     * @param nThreads
      * @return
      */
-    public static ExecutorService newFixedThreadPool(int nThreads, String threadPrefix) {
-        ThreadFactory threadFactory = setThreadFactory(threadPrefix);
-        return Executors.newFixedThreadPool(nThreads, threadFactory);
+    public static ExecutorService newFixedThreadPool(String threadPrefix, int nThreads) {
+        return newThreadPoolExecutor(threadPrefix, nThreads, nThreads, 0L, TimeUnit.MILLISECONDS, QUEUE_SIZE, DEFAULT_HANDLER);
     }
 
     /**
@@ -112,6 +158,12 @@ public class ThreadPoolUtil {
      * @return
      */
     public static ScheduledExecutorService newScheduledThreadPool(int nThreads, String threadPrefix) {
+        if (nThreads < 1) {
+            nThreads = 1;
+        }
+        if (nThreads > MAX_POOL_SIZE) {
+            nThreads = MAX_POOL_SIZE;
+        }
         ThreadFactory threadFactory = setThreadFactory(threadPrefix);
         return Executors.newScheduledThreadPool(nThreads, threadFactory);
     }
@@ -127,9 +179,8 @@ public class ThreadPoolUtil {
      *
      * @param futures
      * @param millis
-     * @throws InterruptedException
      */
-    public static void await(List<Future> futures, long millis) throws InterruptedException {
+    public static void await(List<Future> futures, long millis) {
         if (futures == null || futures.size() == 0) {
             return;
         }
@@ -137,30 +188,100 @@ public class ThreadPoolUtil {
         do {
             for (Future future : futures) {
                 if (!(isDone = future.isDone())) {
-                    Thread.sleep(millis);
+                    SleepUtil.milliseconds(millis);
                     break;
                 }
             }
         } while (!isDone);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        ExecutorService service = newCachedThreadPool("demo");
-        Future future = service.submit(() -> {
-            System.out.println("任务一开始");
-            Thread.sleep(3000);
-            System.out.println("任务一结束");
-            return null;
-        });
-        Future future2 = service.submit(() -> {
-            System.out.println("任务二开始");
-            Thread.sleep(2000);
-            System.out.println("任务二结束");
-            return null;
-        });
-        List<Future> futures = new ArrayList<>(Arrays.asList(future, future2));
-        await(futures, 10000);
-        System.out.println("end");
+    /**
+     * 等待所有任务执行完
+     *
+     * @param service
+     */
+    public static void await(ExecutorService service) {
         service.shutdown();
+        while (!service.isTerminated()) {
+            try {
+                boolean isFinish = service.awaitTermination(10, TimeUnit.SECONDS);
+                if (isFinish) {
+                    break;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    public static List<Runnable> await(ExecutorService service, long timeout, TimeUnit unit) {
+        service.shutdown();
+        while (!service.isTerminated()) {
+            try {
+                boolean isFinish = service.awaitTermination(timeout, unit);
+                if (!isFinish) {
+                    return service.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+        return Collections.emptyList();
+    }
+
+
+    /**
+     * 计算需要多少线程数
+     *
+     * @param taskNum        任务数量
+     * @param avgTaskSecond  平均每个任务耗时（秒）
+     * @param completeSecond 完成所有任务需要的时间
+     * @return
+     */
+    public static int calculatePoolSize(int taskNum, double avgTaskSecond, double completeSecond) {
+        return BigDecimal
+                .valueOf(taskNum / (completeSecond / avgTaskSecond))
+                .setScale(0, BigDecimal.ROUND_UP)
+                .intValue();
+    }
+
+    /**
+     * 计算任务数量
+     *
+     * @param poolSize       线程数
+     * @param avgTaskSecond  平均每个任务耗时（秒）
+     * @param completeSecond 完成所有任务需要的时间
+     * @return
+     */
+    public static int calculateTaskNum(int poolSize, double avgTaskSecond, double completeSecond) {
+        return BigDecimal
+                .valueOf(poolSize * (completeSecond / avgTaskSecond))
+                .setScale(0, BigDecimal.ROUND_UP)
+                .intValue();
+    }
+
+    /**
+     * 计算执行完成时间
+     *
+     * @param poolSize      线程数
+     * @param taskNum       任务数量
+     * @param avgTaskSecond 平均每个任务耗时（秒）
+     * @return
+     */
+    public static double calculateCompleteSecond(int poolSize, int taskNum, double avgTaskSecond) {
+        return BigDecimal
+                .valueOf(avgTaskSecond * (double) (taskNum / poolSize))
+                .setScale(2, BigDecimal.ROUND_UP)
+                .doubleValue();
+    }
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(calculatePoolSize(500, 0.1d, 1));
+        System.out.println((int) Math.ceil((double) 5001 / QUEUE_SIZE));
+        System.out.println(calculateCompleteSecond(10, 100, 0.1d));
     }
 }
